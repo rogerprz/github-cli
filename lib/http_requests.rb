12df7ce5da275bc7ac09c35597e49c6f10eb7532
rogerprz <rogerprz@github.com>
2020-05-28 20:43:26 -0700

    working refactor

--- lib/http_requests.rb
+++ lib/http_requests.rb
@@ -8,36 +8,9 @@ def get_repos
     puts "No repos found!".red
     ARGUMENTS['repos'] = results
   else
+    ARGUMENTS['repos'] = results
     puts "Success! We found #{results.size} repos.".green
   end
-end
-
-def get_repos_with_paging(uri)
-  results = []
-  page = 1
-
-  escaped_base_url = URI::DEFAULT_PARSER.escape("#{uri}&page=#{page}")
-  uri = escaped_base_url.to_s
-  response = get_repos(uri)
-  results.concat(response.fetch(:data))
-  binding.pry
-  # TODO: LINK Example
-  # "<https://api.github.com/user/17ff/repos?per_page=100&page=1>; rel=\"prev\", <https://api.github.com/user/176ffff/repos?per_page=100&page=1>; rel=\"first\""
-  until response.fetch(:data).empty?
-    page += 1
-    uri = "#{uri}&page=#{page}"
-    response = get_repos(uri)
-    binding.pry
-    results.concat(response.fetch(:data))
-  end
-  puts "Success! We found #{results.size} repos.".green
-  ARGUMENTS['repos'] = results
-end
-
-def response_data(response)
-  return unless response.code == '200'
-
-  JSON.parse(response.body)
 end
 
 def remove_repo(full_repo_name)
